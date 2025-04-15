f7d52519cb60d5d07cf9e6c1d74c244b75d1e8d0
Timur <114247583+Braeer@users.noreply.github.com>
2025-04-16 00:51:08 +0300

    ADD: DetailsPage and parse info for this pages, ADD: details reducer, actions, selectors

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,19 +3,33 @@ import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
-
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { selectDetails } from '../store/details/details-selectors';
+import { useEffect } from 'react';
+import { clearDetails, loadCountryByName } from '../store/details/details-action';
 
 export const Details = () => {
   const { name } = useParams();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const currentCountry = null;
+  const { currentCountry, error, status } = useSelector(selectDetails);
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+    return () => {
+      dispatch(clearDetails());
+    };
+  }, [dispatch, name]);
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
+      {status === 'loading' && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
   );
---
+++ src/store/details/details-reducer.js
@@ -0,0 +1,40 @@
+import { CLEAR_DETAILS, SET_COUNTRY, SET_ERROR, SET_LOADING } from './details-action';
+
+const initialState = {
+  currentCountry: null,
+  status: 'idle',
+  error: null,
+};
+
+export const detailsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING: {
+      return {
+        ...state,
+        status: 'loading',
+        error: null,
+      };
+    }
+    case SET_ERROR: {
+      return {
+        ...state,
+        status: 'rejected',
+        error: payload,
+      };
+    }
+    case CLEAR_DETAILS: {
+      return initialState;
+    }
+    case SET_COUNTRY: {
+      return {
+        ...state,
+        status: 'received',
+        currentCountry: payload,
+        error: null,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
